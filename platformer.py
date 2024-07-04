c22bfba6b206fc1ef0fd2838d74b2a64b06461cf
sstm4 <141274393+sstm4@users.noreply.github.com>
2024-07-04 13:05:33 +0100

    added the first edition of the platformer
    this is the edition with velocity ground jumping and that's all

---
+++ platformer.py
@@ -0,0 +1,69 @@
+import pygame, math
+from sys import exit
+
+"""
+upload to github
+make player move left and right with constant velocity
+make the sprites
+"""
+
+pygame.init()
+
+clock = pygame.time.Clock()
+
+SCREENWIDTH = 800
+SCREENHEIGHT = 800
+
+screen = pygame.display.set_mode((SCREENWIDTH,SCREENHEIGHT))
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+DARKRED = (190,0,0)
+BGCOLOUR = (66,155,88)
+PLAYERSIZE = 50
+GRAV = 1
+
+JUMPHEIGHT = -15
+
+class Player(pygame.Rect):
+    def __init__(self):
+        super().__init__(SCREENWIDTH // 2, SCREENHEIGHT // 2,PLAYERSIZE,PLAYERSIZE)
+        self.vy = 0
+        self.vx = 0
+        self.on_ground = False
+    
+    def update(self,screen):
+        self.on_ground = False
+        self.vy += GRAV
+        self.y += self.vy
+        if self.bottom > SCREENHEIGHT:
+            self.bottom = SCREENHEIGHT
+            self.on_ground = True
+            self.vy = 0
+        pygame.draw.rect(screen,WHITE,self)
+    
+    def jump(self):
+        if self.on_ground:
+            self.vy = JUMPHEIGHT
+            self.on_ground = False
+
+player = Player()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.jump()
+    screen.fill(BGCOLOUR)
+    player.update(screen)
+    pygame.display.flip()
+
+    keys = pygame.key.get_pressed()
+    
+    clock.tick(60)
+pygame.quit
+exit()
